5a0db2a9662d60fd0b9d33a16e453ec4fe88cb40
niranjanbala <niranjan.bala@gmail.com>
2023-09-03 16:39:54 +0530

    Update brand-logo.tsx

--- src/components/brand-logo.tsx
+++ src/components/brand-logo.tsx
@@ -3,6 +3,7 @@ import { StaticImage } from "gatsby-plugin-image";
 
 export default function GatsbyDatoLogo() {
   return (
-    <StaticImage src="../images/logo.png" alt="Vimasa Consulting" />
+    <StaticImage src="../images/logo.png" alt="Vimasa Consulting" width={80}
+      height={80}/>
   )
 }
