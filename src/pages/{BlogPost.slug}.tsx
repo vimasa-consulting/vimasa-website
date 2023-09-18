3084ce29602b22239adf5e8a38c4b129b5f11708
niranjanbala <niranjan.bala@gmail.com>
2023-09-18 13:05:53 +0530

    Update {BlogPost.slug}.tsx

--- src/pages/{BlogPost.slug}.tsx
+++ src/pages/{BlogPost.slug}.tsx
@@ -1,1 +1,16 @@
-import BlogPost from "../templates/blog-post"
+import * as React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+
+export default function Page(props: PageProps) {
+  const { page } = props.data
+    return (
+    <Layout>
+      <Box paddingY={5}>
+        <Container width="narrow">
+          <Heading as="h1">{page.title}</Heading>
+        </Container>
+      </Box>
+    </Layout>
+  )
+}
